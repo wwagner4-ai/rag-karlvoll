07ec0cf9ac40942382425eeed54886cd8eeb8bd2
wwagner4 <wwagner4@gmail.com>
2026-03-28 09:51:33 +0100

    finetuned the gui

--- splitpdf/src/splitpdf/webapp.py
+++ splitpdf/src/splitpdf/webapp.py
@@ -11,7 +11,7 @@ app = FastAPI(title="Text Query API")
 async def handle_query(
     text: str = Query(..., description="The text content to process", min_length=1),
 ):
-    print("## received:", text)
+    # print(f"query/text: '{text}'")
     answer = f"Answer to: '{text}'"
     return {
         "received_text": text,
@@ -24,7 +24,13 @@ async def handle_index():
     index_path = Path(__file__).parent / "web" / "index.html"
     text = index_path.read_text()
     text = text.replace("{{port}}", context["port"])
-    print(text)
+    return text
+
+
+@app.get("/jquery.js", response_class=HTMLResponse)
+async def handle_jquiry():
+    path_ = Path(__file__).parent / "web" / "jquery-4.0.0.min.js"
+    text = path_.read_text()
     return text
 
 
