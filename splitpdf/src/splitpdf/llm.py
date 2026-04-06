ec96cea3751ea13184af3ba8f6211b4c712ac27f
wwagner4 <wwagner4@gmail.com>
2026-04-06 19:04:53 +0200

    calling llm together

--- splitpdf/src/splitpdf/llm.py
+++ splitpdf/src/splitpdf/llm.py
@@ -1,3 +1,27 @@
-def call_llm(prompt: str) -> str:
-    answer = f"LLM antwortet auf '{prompt}'"
-    return answer
+from together import Together
+
+
+class Llm:
+    _client: Together | None = None
+    _key: str | None = None
+
+    def __init__(self, key: str):
+        self._key = key
+
+    def close(self) -> None:
+        if self._client:
+            self._client.close()
+
+    def query(self, prompt: str) -> list[str]:
+        response = self._get_client().chat.completions.create(
+            model="meta-llama/Llama-3.3-70B-Instruct-Turbo",
+            messages=[{"role": "user", "content": prompt}],
+        )
+        return [choise.message.content for choise in response.choices]
+
+    def _get_client(self) -> Together:
+        if self._client is None:
+            print("#### using key:", self._key)
+            self._client = Together(api_key=self._key)
+
+        return self._client
--- splitpdf/src/splitpdf/webapp.py
+++ splitpdf/src/splitpdf/webapp.py
@@ -2,39 +2,56 @@ from pathlib import Path
 from fastapi import FastAPI, Query
 from fastapi.responses import HTMLResponse
 import uvicorn
+import os
 from contextlib import asynccontextmanager
+from dotenv import load_dotenv
 
 import splitpdf.embed as embed_
+import splitpdf.llm as llm_
 import splitpdf.helper as hlp_
 
 
+load_dotenv()
+
 vector_database = embed_.VectorDatabase()
+
+llm = llm_.Llm(key=os.environ.get("TOGETHER_KEY"))
 
 
 @asynccontextmanager
 async def lifespan(app: FastAPI):
     yield
     print("Shutting down")
-    print("- Closing vector database")
     vector_database.close()
+    llm.close()
 
 
 context = {}
 app = FastAPI(title="Text Query API", lifespan=lifespan)
 
 
+def query_vector_database(prompt: str) -> list[str]:
+    def to_docstring(index: int, doc: embed_.Document) -> str:
+        return f"{index:4d} {doc.page_number:4d} - {doc.text}"
+
+    documents = vector_database.query(hlp_.COLLECTION_NAME, prompt)
+    return [to_docstring(i, d) for i, d in enumerate(documents)]
+
+
+def query_llm(prompt: str) -> list[str]:
+    return llm.query(prompt)
+
+
 @app.get("/query")
 async def handle_query(
     text: str = Query(..., description="The text content to process", min_length=1),
 ):
-    def to_docstring(index: int, doc: embed_.Document) -> str:
-        return f"{index:4d} {doc.page_number:4d} - {doc.text}"
-
-    documents = vector_database.query(hlp_.COLLECTION_NAME, text)
-    docs_text = "</br></br>".join([to_docstring(i, d) for i, d in enumerate(documents)])
+    # answer = query_vector_database(text)
+    answer = query_llm(text)
+    html_answer = "</br></br>".join(answer)
     return {
         "received_text": text,
-        "answer": docs_text,
+        "answer": html_answer,
     }
 
 
