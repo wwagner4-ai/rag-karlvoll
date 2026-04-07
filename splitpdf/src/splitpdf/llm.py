a8a837f4f563301d7ea52779e1a1cf956716d232
wwagner4 <wwagner4@gmail.com>
2026-04-08 00:22:42 +0200

    system prompt

--- splitpdf/src/splitpdf/llm.py
+++ splitpdf/src/splitpdf/llm.py
@@ -12,16 +12,26 @@ class Llm:
         if self._client:
             self._client.close()
 
+    def system_prompt(self) -> str | None:
+        return "Ausgabe als Text ohne Formatierung"
+
     def query(self, prompt: str) -> list[str]:
+        sp = self.system_prompt()
+        if sp:
+            content = f"""
+                {prompt}.\n{sp}
+            """.strip()
+        else:
+            content = prompt
+
         response = self._get_client().chat.completions.create(
             model="meta-llama/Llama-3.3-70B-Instruct-Turbo",
-            messages=[{"role": "user", "content": prompt}],
+            messages=[{"role": "user", "content": content}],
         )
         return [choise.message.content for choise in response.choices]
 
     def _get_client(self) -> Together:
         if self._client is None:
-            print("#### using key:", self._key)
             self._client = Together(api_key=self._key)
 
         return self._client
